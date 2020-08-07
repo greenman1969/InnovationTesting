6e9d5747f4a55fbdbabb7f631ac577d75681f0f6
okh252 <54654991+okh252@users.noreply.github.com>
2020-08-07 13:20:44 -0500

    Update InnovationTestingFlask.py

--- InnovationTestingFlask.py
+++ InnovationTestingFlask.py
@@ -1,1 +1,15 @@
+from flask import Flask
+app = Flask(__name__)
 
+
+@app.route('/')
+def function():
+    return "Enter Gross Domestic Profit (GDP) in US Dollars to see an estimate of COVID-19 Deaths."
+
+
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello {}!".format(name)
+
+if __name__ == '__main__':
+    app.run()
