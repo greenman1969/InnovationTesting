89b65a8ff84c1e55d041b347da0619d89d04db57
Greenhouse <Gage.Greenhouse@conocophillips.com>
2020-08-07 13:32:00 -0500

    Added data to the Python Flask App

--- InnovationTestingFlask.py
+++ InnovationTestingFlask.py
@@ -1,15 +1,30 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
+homePage = '''<!DOCTYPE html>
+            <html>
+                <head>
+                    <title>Innovation Challenge Team 6</title>
+                </head>
+                <body>
+                    <p>Enter Gross Dometic Product (GDP) in US Dollars bellow:</p>
+                    <form method="POST" action="/">
+                        <input name="gdp">
+                        <input type="submit">
+                    </form>
+                </body>
+            </html>'''
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def function():
-    return "Enter Gross Domestic Profit (GDP) in US Dollars to see an estimate of COVID-19 Deaths."
+    if request.method='GET':
+        return homePage
+    else if request.method='POST':
+        return "Return the estimated value"
 
-
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello {}!".format(name)
+#@app.route('/<name>')
+#def hello_name(name):
+#    return "Hello {}!".format(name)
 
 if __name__ == '__main__':
     app.run()
