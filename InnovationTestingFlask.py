152b1502bcebbd1ac77afa783f367328bb30fba7
windowsuser3 <windowsuser3@CORP.AMAZONWORKSPACES.COM>
2020-08-21 11:16:56 -0500

    next Test

--- InnovationTestingFlask.py
+++ InnovationTestingFlask.py
@@ -45,31 +45,31 @@ homePage = '''<!DOCTYPE html>
                 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
                 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
                 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-                    <!--<style>
-                        html {
-				            margin: auto; 
-			            	width: 50%; 
-			            	background-color: ##a9e8a9;
-                            border-style:solid;
-				            border-color:grey;
-				            border-width: 1.5px;
-				        }
-				
-                        h1 {
-				            color:#033c8c;
-                      		font-size: 20px;
-				            text-align: center;
-                        }
-                    </style>-->
                 </head>
                 <body>
                     <h1> Innovation Challenge Team 5 </h1>
                     <p>Enter Gross Dometic Product (GDP) in US Dollars and population below:</p>
                     <form method="POST" action="/">
-                        <input type="number" name="gdp" placeholder="GDP">
-			            <input type="number" name="pop" placeholder="Population">
+                        <input type="number" name="gdp" placeholder="GDP" min="0">
+			            <input type="number" name="pop" placeholder="Population" min="0">
                         <input type="submit">
                     </form>
+                </body>
+            </html>'''
+
+def returnValue(value, accuracy=0):
+    homePage = '''<!DOCTYPE html>
+            <html>
+                <head>
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+                <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+                </head>
+                <body>
+                    <h1> Innovation Challenge Team 5 </h1>
+                    <p>Your expected deaths are: '''+str(value)+'''</p>
                 </body>
             </html>'''
 
@@ -78,7 +78,14 @@ def function():
     if request.method=='GET':
         return homePage
     elif request.method=='POST':
-        return "Return the estimated value"
+        gdp = request.form['gdp']
+        pop = request.form['pop']
+        features = np.array([[gdp, pop]])
+        prediction = reg.predict(features)
+        print(prediction)
+
+
+        return returnValue(prediction)
 
 #@app.route('/<name>')
 #def hello_name(name):
