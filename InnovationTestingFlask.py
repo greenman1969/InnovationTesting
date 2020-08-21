3e5e913f5e2c20c22eeae35c6ece66143c6936ad
okh252 <54654991+okh252@users.noreply.github.com>
2020-08-21 10:25:40 -0500

    Update InnovationTestingFlask.py

--- InnovationTestingFlask.py
+++ InnovationTestingFlask.py
@@ -6,9 +6,14 @@ app = Flask(__name__)
 homePage = '''<!DOCTYPE html>
             <html>
                 <head>
-                    <title>Innovation Challenge Team 6</title>
+                    <style>
+                        body {background-color: #32a852; opacity: .3; border-style:solid; border-color:grey;}
+                        h1 {color:#033c8c; font-size: 20px;}
+                        
+                    </style> 
                 </head>
                 <body>
+                    <h1> Innovation Challenge Team 5 </h1> 
                     <p>Enter Gross Dometic Product (GDP) in US Dollars bellow:</p>
                     <form method="POST" action="/">
                         <input name="gdp">
